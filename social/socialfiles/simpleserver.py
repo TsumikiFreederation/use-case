50272a2a764e2b6172cb87d30ed974f952b1b2a1
yyzhuang <yanyanzhuang83@gmail.com>
2014-09-17 14:32:22 -0700

    fix friend socket port reuse

---
+++ social/socialfiles/simpleserver.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import socket
+import datetime
+import time
+
+TCP_IP = '198.162.52.146'
+TCP_PORT = 63109
+BUFFER_SIZE = 64  # Normally 1024, but we want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(5)
+
+while 1:
+  conn, addr = s.accept()
+  data = conn.recv(BUFFER_SIZE)
+  ts = time.time()  # time stamp
+  dt = datetime.datetime.now()  # date time format
+  print str(ts), '\t', str(dt), " from ", data
+  conn.close()
